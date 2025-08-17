0f06173676d9c9a4d564b48bc93621c3e6e06d6a
avinash-218 <ravinash218@gmail.com>
2025-08-17 17:15:47 +0530

    done

--- CrewAI-101/2/1_news_scrapper/src/news_scrapper/main.py
+++ CrewAI-101/2/1_news_scrapper/src/news_scrapper/main.py
@@ -22,48 +22,22 @@ def run():
         'topic': 'AI LLMs',
         'current_year': str(datetime.now().year)
     }
+
+    inputs_array = [
+        {
+            "topic": "AI Agents",
+            "date": datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        },
+        {
+            "topic": "Model Context Protocol",
+            "date": datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        },
+        {
+            "topic": "Agent to Agent Communication",
+            "date": datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        }
+    ]
     
-    try:
-        NewsScrapper().crew().kickoff(inputs=inputs)
-    except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        NewsScrapper().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        NewsScrapper().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    try:
-        NewsScrapper().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
+    NewsScrapper().crew().kickoff(inputs=inputs)
 
 run()
